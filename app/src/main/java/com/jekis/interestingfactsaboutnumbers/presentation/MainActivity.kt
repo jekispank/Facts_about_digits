ef0298ec212ba55d5eaf34c99ea138e08fe66112
Evgeny <jekispank@mail.ru>
2023-02-18 23:47:33 +0300

    Creating UI for MainActivity

---
+++ app/src/main/java/com/jekis/interestingfactsaboutnumbers/presentation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.jekis.interestingfactsaboutnumbers.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.jekis.interestingfactsaboutnumbers.R
+import com.jekis.interestingfactsaboutnumbers.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/jekis/interestingfactsaboutnumbers/presentation/StartFragment.kt
@@ -0,0 +1,25 @@
+package com.jekis.interestingfactsaboutnumbers.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jekis.interestingfactsaboutnumbers.R
+import com.jekis.interestingfactsaboutnumbers.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+    private lateinit var binding: FragmentStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
